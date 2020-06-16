1b47214dac689899a896a485442d159a2ec85c41
Your Name <diego-vespa2020@outlook.com.br>
2020-06-16 19:53:36 -0300

    treinos youtube

---
+++ pag/js/script.js
@@ -0,0 +1,9 @@
+function toggle() {
+  console.log('clicquei')
+  var trailer = document.querySelector('.trailer');
+  var video = document.querySelector('video');
+  trailer.classList.toggle('active');
+  video.currentTime = 0;
+  video.pause();
+}
+
