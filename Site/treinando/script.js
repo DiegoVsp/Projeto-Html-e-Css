e013e149b5e9a8730a3129135599a217e62688bc
Your Name <diego-vespa2020@outlook.com.br>
2020-08-16 14:48:08 -0300

    att

---
+++ Site/treinando/script.js
@@ -0,0 +1,6 @@
+function menuAtiva() {
+  const nav = document.querySelector('.nav');
+  const toggle = document.querySelector('.toggle')
+  nav.classList.toggle('ativo')
+  toggle.classList.toggle('ativo')
+}
