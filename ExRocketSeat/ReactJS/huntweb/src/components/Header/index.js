50431c0496c9c86cef7c19787e72554634365447
Diego Vespa <diego-vespa2020@outlook.com.br>
2020-08-31 17:40:33 -0300

    atualizando Projeto

---
+++ ExRocketSeat/ReactJS/huntweb/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+// quando se tem um retorno logo após as chaves, pode-se usar
+// somente os parenteses que vai ser aceito
+const Header = () => (
+  <header id="main-header">
+    JSHUNT
+  </header>
+);
+
+export default Header;
